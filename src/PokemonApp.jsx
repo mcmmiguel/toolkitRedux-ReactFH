f8f2910be029515af7b94c9f58120bf9e7d78aa2
Miguel Cobian <miguelcobian99@gmail.com>
2023-10-12 13:42:29 -0600

    Add. PokemonSlice created

---
+++ src/PokemonApp.jsx
@@ -0,0 +1,15 @@
+
+export const PokemonApp = () => {
+    return (
+        <>
+            <h1>PokemonApp</h1>
+            <hr />
+
+            <ul>
+                <li>Pokemamon</li>
+                <li>Pokemamon</li>
+                <li>Pokemamon</li>
+            </ul>
+        </>
+    )
+}
