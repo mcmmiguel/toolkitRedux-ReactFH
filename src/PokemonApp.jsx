28e3e4427bee490430c8b259145d9a4136e9ca3e
Miguel Cobian <miguelcobian99@gmail.com>
2023-10-12 14:48:25 -0600

    Feat. Thunk created and implemented with Axios

--- src/PokemonApp.jsx
+++ src/PokemonApp.jsx
@@ -1,5 +1,19 @@
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { getPokemons } from './store/slices/pokemon/thunks';
 
 export const PokemonApp = () => {
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+
+        dispatch(getPokemons());
+
+    }, [])
+
+
+
     return (
         <>
             <h1>PokemonApp</h1>
