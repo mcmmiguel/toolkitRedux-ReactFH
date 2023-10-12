176317cbbcca0cbd9eb2ec950697ebdcf83029e0
Miguel Cobian <miguelcobian99@gmail.com>
2023-10-12 13:01:25 -0600

    Feat. Decrement and IncrementBy actions were implemented

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useSelector, useDispatch } from 'react-redux'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { increment } from './store/slices/counter';
+import { decrement, increment, incrementBy } from './store/slices/counter';
 function App() {
 
   const { counter } = useSelector(state => state.counter);
@@ -18,10 +18,16 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>count is {counter}</h1>
       <div className="card">
         <button onClick={() => dispatch(increment())}>
-          count is {counter}
+          Increment
+        </button>
+        <button onClick={() => dispatch(decrement())}>
+          Decrement
+        </button>
+        <button onClick={() => dispatch(incrementBy(2))}>
+          Increment by 2
         </button>
       </div>
     </>
