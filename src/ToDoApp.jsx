52b2c2a7e3e7794b22551a1421d28268aa9331df
Miguel Cobian <miguelcobian99@gmail.com>
2023-10-12 18:58:09 -0600

    Add. RTK Query created

---
+++ src/ToDoApp.jsx
@@ -0,0 +1,16 @@
+
+
+export const ToDoApp = () => {
+    return (
+        <>
+            <h1>ToDos - RTK Query</h1>
+            <hr />
+            <h4>isLoading...</h4>
+
+            <pre></pre>
+
+            <button>Next ToDo</button>
+
+        </>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,13 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { store } from './store'
 import { Provider } from 'react-redux'
-import { PokemonApp } from './PokemonApp.jsx'
+// import { PokemonApp } from './PokemonApp.jsx'
+import { ToDoApp } from './ToDoApp'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <PokemonApp />
+      <ToDoApp />
     </Provider>
   </React.StrictMode>,
 )
