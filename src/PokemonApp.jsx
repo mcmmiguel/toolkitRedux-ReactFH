f6c3c41e21f48bbf5fa801e01b06815637b08383
Miguel Cobian <miguelcobian99@gmail.com>
2023-10-12 18:44:23 -0600

    Feat. Consuming the fetched data

--- src/PokemonApp.jsx
+++ src/PokemonApp.jsx
@@ -1,10 +1,11 @@
 import { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { getPokemons } from './store/slices/pokemon/thunks';
 
 export const PokemonApp = () => {
 
     const dispatch = useDispatch();
+    const { pokemons, isLoading, page } = useSelector(state => state.pokemons);
 
     useEffect(() => {
 
@@ -18,12 +19,22 @@ export const PokemonApp = () => {
         <>
             <h1>PokemonApp</h1>
             <hr />
+            <span>Loading: {`${isLoading}`}</span>
 
             <ul>
-                <li>Pokemamon</li>
-                <li>Pokemamon</li>
-                <li>Pokemamon</li>
+                {
+                    pokemons.map(pokemon => (
+                        <li key={pokemon.name}>{pokemon.name}</li>
+                    ))
+                }
             </ul>
+
+            <button
+                disabled={isLoading}
+                onClick={() => dispatch(getPokemons(page))}
+            >
+                Next
+            </button>
         </>
     )
 }
